176956b12b7a75040561cdc4f995058f3ec23dc9
KJAstudios <kja@surewest.net>
2020-11-12 15:41:52 -0800

    Class 11/12/20

--- MyThreeDGame.py
+++ MyThreeDGame.py
@@ -2,63 +2,46 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from SlowCube import SlowCube
 
+# Main Init
 pygame.init()
 size = width, height = 640, 480
 screen = pygame.display.set_mode(size, DOUBLEBUF | OPENGL)
 
+glMatrixMode(GL_PROJECTION)
 gluPerspective(45, (width / height), 0.1, 50.0)
+glMatrixMode(GL_MODELVIEW)
 
 glEnable(GL_DEPTH_TEST)
 glDepthFunc(GL_LESS)
+
 glTranslate(0.0, 0.0, -5)
 
-verts = ((1, -1, -1),
-         (1, 1, -1),
-         (-1, 1, -1),
-         (-1, -1, -1),
-         (1, -1, 1),
-         (1, 1, 1),
-         (-1, -1, 1),
-         (-1, 1, 1))
+cube = SlowCube()
 
-surfaces = ((0, 1, 2, 3),
-            (3, 2, 7, 6),
-            (6, 7, 5, 4),
-            (4, 5, 1, 0),
-            (1, 5, 7, 2),
-            (4, 0, 3, 6))
 
-colors = ((0, 0, 1),
-          (0, 1, 0),
-          (0, 0, 1),
-          (0, 1, 0))
-
-ang = 0
-
-clock = pygame.time.Clock()
-gameQuit = False
-while not gameQuit:
+def Update(deltaTime):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            gameQuit = True
+            return False
 
-    deltaTime = float(clock.tick(60)) / 1000
+    cube.Update(deltaTime)
+    return True
 
-    ang += 50.0 * deltaTime
 
+def Render():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    m = glGetDouble(GL_MODELVIEW_MATRIX)
 
-    glRotatef(ang, 3, 1, 1)
-    glBegin(GL_QUADS)
-    for surface in surfaces:
-        for i, vert in enumerate(surface):
-            glColor3fv(colors[i])
-            glVertex3fv(verts[vert])
-    glEnd()
-
-    glLoadMatrixf(m)
+    cube.Render()
 
     pygame.display.flip()
 
+
+_gTickLastFrame = pygame.time.get_ticks()
+_gDeltaTime = 0.0
+while Update(_gDeltaTime):
+    Render()
+    t = pygame.time.get_ticks()
+    _gDeltaTime = (t - _gTickLastFrame) / 1000.0
+    _gTickLastFrame = t
