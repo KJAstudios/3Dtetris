0517f67de06d5ffe8c3f28036096dfc8d9676815
Daniel Nguyen <dung.nguyen@jessup.edu>
2020-11-20 15:17:41 -0800

    UI Text

--- UI/UIText.py
+++ UI/UIText.py
@@ -2,61 +2,57 @@ import numpy as np
 import pygame
 # from World.WorldCommon import ScreenSize
 from UI.UIImage import UIImage
+import freetype
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
+Characters = dict()
+class Character:
 
-class UIText(UIImage):
+    def __init__(self, texture, glyph):
+        self.texture = texture
+        self.textureSize = (glyph.bitmap.width, glyph.bitmap.rows)
 
-    def __init__(self, element=None):
-        if element != None:
-            super().__init__(element=element)
-            txt_el = element.find("Font")
-            self.text = txt_el.get("text", "")
-            self.font_name = txt_el.get("font", "arial")
-            self.color_name = txt_el.get("color", "white")
-            self.size = int(txt_el.get("size", 16))
-            self.txt_justify = txt_el.get("justify")
-            self.txt_vjustify = txt_el.get("vjustify")
-
-            self.color = pygame.Color(self.color_name)
-            self.font = pygame.font.SysFont(self.font_name, self.size)
-            self.txt_surf = self.font.render(self.text, True, self.color)
-            self.txt_width = self.txt_surf.get_width()
-            self.txt_height = self.txt_surf.get_height()
-            self.txt_rect = pygame.Rect((self.x, self.y), (self.txt_width, self.txt_height))
-            self._CalcTextRect()
+        if isinstance(glyph, freetype.GlyphSlot):
+            self.bearing = (glyph.bitmap_left, glyph.bitmap_top)
+            self.advance = glyph.advance.x
+        elif isinstance(glyph, freetype.BitmapGlyph):
+            self.bearing = (glyph.left, glyph.top)
+            self.advance = None
         else:
-            self.txt_surf = None
-            self.txt_width = 0
-            self.txt_height = 0
-        self.txt_rect = pygame.Rect((self.x, self.y), (self.txt_width, self.txt_height))
-        self._CalcTextRect()
+            raise RuntimeError('unknown glyph type')
 
-    def _CalcTextRect(self):
-        self._CalcRect()
 
-        self.txt_rect.left = self.rect.left
-        if self.txt_justify == "right":
-            self.txt_rect.left += self.width - self.txt_width
-        if self.txt_justify == "center":
-            self.txt_rect.left += (self.width - self.txt_width) // 2
 
-        self.txt_rect.top = self.rect.top
-        if self.txt_vjustify == "bottom":
-            self.txt_rect.top += self.height - self.txt_height
-        if self.txt_vjustify == "center":
-            self.txt_rect.top += (self.height - self.txt_height) // 2
+fontfile="C:\Windows\Fonts\Arial.ttf"
+def Init():
+    global Characters
+    face = freetype.Face(fontfile)
+    face.set_char_size(48 * 64)
+    # load first 128 characters of ASCII set
+    for i in range(0, 128):
+        face.load_char(chr(i))
+        glyph = face.glyph
 
-        self.txt_rect.width = self.txt_width
-        self.txt_rect.height = self.txt_height
+        # generate texture
+        texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, glyph.bitmap.width, glyph.bitmap.rows, 0,
+                     GL_RED, GL_UNSIGNED_BYTE, glyph.bitmap.buffer)
 
-    def Update(self, deltaTime):
-        super().Update(deltaTime)
+        # texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
-    def Render(self, screen):
-        if self.visible:
-            super().Render(screen)
-            if self.txt_surf != None:
-                screen.blit(self.txt_surf, self.txt_rect)
+        # now store character for later use
+        Characters[chr(i)] = Character(texture, glyph)
+
+    glBindTexture(GL_TEXTURE_2D, 0)
+
+
+
 
 
 
